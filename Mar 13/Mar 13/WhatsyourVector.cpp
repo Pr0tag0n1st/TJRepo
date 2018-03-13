89b36d5cdc12c2c595ffe5cc7c590540d93b65ac
Pr0tag0n1st <Tj_Martinez01@hotmail.com>
2018-03-13 08:13:42 -0600

    Now if only my library worked

---
+++ Mar 13/Mar 13/WhatsyourVector.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int input;
+	vector<int> Victor;
+	vector<int>::iterator iter;
+	cout << "Give me a number other than 0:" << endl;
+	cin >> input;
+	Victor.push_back(input);
+	while (input != 0) {
+		cout << "Another one:" << endl;
+		cin >> input;
+		Victor.push_back(input);
+	}
+	cout << "Here are your numbers:" << endl;
+	for (iter = Victor.begin(); iter != Victor.end(); ++iter)
+		cout << *iter << endl;
+	sort(Victor.begin(), Victor.end(), Victor);
+	cout << "In order, they are:" << endl;
+	for (iter = Victor.begin(); iter != Victor.end(); ++iter)
+		cout << *iter << endl;
+	
+}
