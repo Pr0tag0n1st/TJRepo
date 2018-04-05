12c254bd26ce19b02411c3c5890c4f427606fa25
Pr0tag0n1st <Tj_Martinez01@hotmail.com>
2018-04-05 08:30:24 -0600

    Still broken despite our best efforts

--- Mar 22 + 23/Mar 22 + 23/StudentsandFaces.cpp
+++ Mar 22 + 23/Mar 22 + 23/StudentsandFaces.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
+#include<allegro5\allegro_primitives.h>
 using namespace std;
 class FACE {
 public:
@@ -22,17 +23,18 @@ int main() {
 	al_init();
 	al_init_image_addon();
 	ALLEGRO_DISPLAY*display = al_create_display(600, 400);
-	cout << "flag1" << endl;
 	ALLEGRO_BITMAP*mario = al_load_bitmap("Mayo.png");
 	ALLEGRO_BITMAP*luigi = al_load_bitmap("Linguini.png");
 	ALLEGRO_BITMAP*link = al_load_bitmap("Stink.png");
 	ALLEGRO_BITMAP*samus = al_load_bitmap("Spamus.png");
 	ALLEGRO_BITMAP*falcon = al_load_bitmap("Hyes.png");
 	ALLEGRO_BITMAP*fox = al_load_bitmap("Toriyah.png");
+	if (fox == NULL)
+		cout << "fox is null!" << endl;
+
 	ALLEGRO_TIMER*timer = al_create_timer(0.02);
 	ALLEGRO_EVENT_QUEUE*event_queue = al_create_event_queue();
 	bool redraw = true;
-	cout << "Flag2" << endl;
 	al_register_event_source(event_queue, al_get_display_event_source(display));
 	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 	al_set_target_backbuffer(display);
@@ -43,7 +45,6 @@ int main() {
 	FACE face4;
 	FACE face5;
 	FACE face6;
-	cout << "Flag3" << endl;
 	face1.initFace(25, 75, 2, 2, mario);
 	face2.initFace(75, 100, 2, 4, luigi);
 	face3.initFace(50, 50, 4, 2, link);
@@ -57,33 +58,42 @@ int main() {
 	faces.push_back(face4);
 	faces.push_back(face5);
 	faces.push_back(face6);
-	cout << "flag4" << endl;
+	al_draw_bitmap(luigi, 10, 10, 0);
+	al_flip_display();
+	system("Pause");
 	al_start_timer(timer);
 	while (1) {
-		cout << "flag4.5" << endl;
+
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
-		cout << "flag4.75" << endl;
 		if (ev.type == ALLEGRO_EVENT_TIMER) {
-			cout << "flag5" << endl;
+
+			//walks through vector, prints and moves each face
 			for (iter = faces.begin(); iter != faces.end(); iter++) {
+				iter->printFace();
 				iter->Movement();
 				iter->printFace();
 			}
-			cout << "flag6" << endl;
 			redraw = true;
 		}
 		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
 			break;
 		}
+
+		//render section
 		if (redraw && al_is_event_queue_empty(event_queue)) {
 			redraw = false;
-			cout << "flag7" << endl;
+			cout << "flag1" << endl;
 			al_clear_to_color(al_map_rgb(0, 0, 0));
+			cout << "Flag2" << endl;
 			for (iter = faces.begin(); iter != faces.end(); iter++) {
+				cout << "flag3" << endl;
 				iter->drawFace();
+				cout << "flag4";
+				iter->printFace();
 			}
 			al_flip_display();
+			cout << "Flag4" << endl;
 		}
 	}
 
@@ -97,12 +107,28 @@ void FACE::initFace(int x, int y, int movex, int movey, ALLEGRO_BITMAP*sample) {
 	dy = movey;
 	ALLEGRO_BITMAP*pic = sample;
 }
+
+
 void FACE::drawFace() {
-	al_draw_bitmap(pic, xpos, ypos, 0);
+	cout << "inside drawface" << endl;
+	cout << "position is " << xpos << " , " << ypos << endl;
+
+	if (pic == NULL)
+		cout << "pic never loaded :(" << endl;
+
+	//al_draw_bitmap(pic, xpos, ypos, 0);
+	cout << "drawing stuff" << endl;
+	al_draw_circle(xpos, ypos, 100, al_map_rgb(50, 50, 200), 10);
+
+	cout << "exiting drawface" << endl;
 }
+
+
 void FACE::printFace() {
 	cout << "Face is at:" << xpos << ", " << ypos << ", going " << dx << ", " << dy << " units per frame." << endl;
 }
+
+
 void FACE::Movement() {
 	xpos += dx;
 	ypos += dy;
